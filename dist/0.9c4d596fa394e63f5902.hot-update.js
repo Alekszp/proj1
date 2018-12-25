8d3ace9cd01edc6728d03bb8320acde9fe863cde
Alex <Alex@DESKTOP-N9TJAN3>
2018-12-25 18:54:51 +0200

    commit init

---
+++ dist/0.9c4d596fa394e63f5902.hot-update.js
@@ -0,0 +1,1 @@
+webpackHotUpdate(0,{8:function(e,n,t){"use strict";t.r(n);var l=t(0),a=t.n(l),r=t(2),c=function(){return a.a.createElement("div",null,a.a.createElement("p",null,"Hello, REACT!!!!!"))};t.n(r).a.render(a.a.createElement(c,null),document.getElementById("app"))}});
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=> {
+    return (
+        <div>
+            <p>Hello, REACT!!!!!</p>
+        </div>
+    );
+};
+
+export default App;
+
+ReactDOM.render(
+    < App/>,
+    document.getElementById('app')
+);
